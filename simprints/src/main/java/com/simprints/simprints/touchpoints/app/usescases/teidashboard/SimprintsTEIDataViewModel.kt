2107bd0cd2479b907ca421ef60a2904938600d18
Tristram Norman <tristram@simprints.com>
2024-06-09 20:21:50 +0100

    remove un-used imports

--- simprints/src/main/java/com/simprints/simprints/touchpoints/app/usescases/teidashboard/SimprintsTEIDataViewModel.kt
+++ simprints/src/main/java/com/simprints/simprints/touchpoints/app/usescases/teidashboard/SimprintsTEIDataViewModel.kt
@@ -1,13 +1,10 @@
 package com.simprints.simprints.touchpoints.app.usescases.teidashboard
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.simprints.simprints.repository.SimprintsBiometricsRepository
-import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 class SimprintsTEIDataViewModel (
         private val simprintsBiometricsRepository: SimprintsBiometricsRepository
